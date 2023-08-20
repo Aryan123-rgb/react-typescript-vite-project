340db7bc28a28cbcb3b1cf76e7f67897687375a5
Aryan@123-rgb <srivatavaaryanalc76@gmail.com>
2023-08-20 13:12:11 +0530

    a

--- src/components/SecondPage.tsx
+++ src/components/SecondPage.tsx
@@ -2,7 +2,6 @@ import React, { useEffect, useState } from "react";
 import { DataGrid, GridColDef } from "@mui/x-data-grid";
 import DepartmentList from "./DepartmentList";
 import { Typography } from "@mui/material";
-import { useNavigate } from "react-router-dom";
 
 function SecondPage() {
   interface Post {
@@ -12,8 +11,6 @@ function SecondPage() {
     body: string;
   }
   const [posts, setPosts] = useState<Post[]>([]);
-  const [userDetailsPresent, setUserDetailsPresent] = useState(true);
-  const navigate = useNavigate();
 
   const fetchPosts = async () => {
     try {
@@ -29,24 +26,9 @@ function SecondPage() {
     }
   };
 
-  // const checkUserDetailsPresent = () => 
-  //   const userDetails = localStorage.getItem('userDetails');
-  //   console.log(userDetails);
-  //   if(!userDetails){
-  //     setUserDetailsPresent(false);
-  //   }
-  //   else setUserDetailsPresent(true);
-  // }
-
   useEffect(() => {
     fetchPosts();
-    // checkUserDetailsPresent();
   }, []);
-
-
-  // if(userDetailsPresent === false){
-  //   navigate('/');
-  // }
 
   const columns: GridColDef[] = [
     { field: "id", headerName: "ID", width: 120 },
